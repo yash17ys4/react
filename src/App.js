636dcac8e398fb31e8c31ececa305b6b46d93400
yash17ys4 <yash17.ys4@gmail.com>
2020-05-07 20:39:56 +0530

    form created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form/Form';
 
 function App() {
   return (
     <div className="App">
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+      height: '50vh',
+      width: '50vh',
+      position: 'fixed',
+      padding: '5%',
+      backgroundColor: '#bfb8f0',
+      border: '#bf08f3 1% solid'
+  },
+}));
+
+export default function Form() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} autoComplete="off">
+      <TextField id="text" label="Match" />
+      <TextField
+          id="number"
+          label="Score"
+          type="number"
+        />
+      <TextField id="standard-basic" label="Name of Opponent" />
+      <TextField id="standard-basic" label="Comment" />
+      <InputLabel id="demo-simple-select-label">Type of game</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+        //   value={type}
+        //   onChange={handleChange}
+        >
+          <MenuItem value={0}>Practice</MenuItem>
+          <MenuItem value={1}>Tournament</MenuItem>
+          <MenuItem value={2}>Training Camp</MenuItem>
+        </Select>
+      <Button>Submit</Button>
+    </form>
+  );
+}
